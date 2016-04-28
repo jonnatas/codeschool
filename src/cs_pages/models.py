0d8f19c079bd01af7597d010e9ed572c70e63183
jonnatas <jonatas_lenon@hotmail.com>
2016-04-27 21:32:36 -0300

    tentando implementar reconhecimento de código

--- src/cs_pages/models.py
+++ src/cs_pages/models.py
@@ -6,10 +6,49 @@ from wagtail.wagtailcore import blocks
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 
+from pygments import highlight
+from pygments.formatters import get_formatter_by_name
+from pygments.lexers import get_lexer_by_name
+
+
+from django.utils.safestring import mark_safe
+class CodeBlock(blocks.StructBlock):
+    """
+    Code Highlighting Block
+    """
+    LANGUAGE_CHOICES = (
+        ('python', 'Python'),
+        ('bash', 'Bash/Shell'),
+        ('html', 'HTML'),
+        ('css', 'CSS'),
+        ('scss', 'SCSS'),
+    )
+
+    language = blocks.ChoiceBlock(choices=LANGUAGE_CHOICES)
+    code = blocks.TextBlock()
+
+    class Meta:
+        icon = 'code'
+
+    def render(self, value):
+        src = value['code'].strip('\n')
+        lang = value['language']
+
+        lexer = get_lexer_by_name(lang)
+        formatter = get_formatter_by_name(
+            'html',
+            linenos=None,
+            cssclass='codehilite',
+            style='default',
+            noclasses=False,
+        )
+        return mark_safe(highlight(src, lexer, formatter))
+
 class HomePage(Page):
     body = StreamField([
         ('heading', blocks.CharBlock(classname="full title")),
         ('paragraph', blocks.RichTextBlock()),
+        ('code', CodeBlock()),
         ('image', ImageChooserBlock()),
     ])
 
@@ -18,6 +57,3 @@ class HomePage(Page):
     ]
 
     template = 'cs_pages/home_page.jinja2'
-
-
-
