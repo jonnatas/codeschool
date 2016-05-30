cc3454d7f1b042be5a0c46c87a4333553624df36
Marcelo <marceloabk@hotmail.com>
2016-05-30 17:10:29 -0300

    interactive code working with skulpt

--- src/cs_pages/models.py
+++ src/cs_pages/models.py
@@ -73,11 +73,13 @@ class InputCodeBlock(CodeBlock):
 
 class userInputBlock(CodeBlock):
     """interative code blocks """
+
+    expected_result = blocks.TextBlock()
     
     def render(self, value):
         lang = 'python'
         code = escape(value['code'])
-        data = '<script type="text/javascript"> function outf(text) { var mypre = document.getElementById("output"); mypre.innerHTML = mypre.innerHTML + text; } function builtinRead(x) { if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined) throw "File not found: '" + x + "'"; return Sk.builtinFiles["files"][x];}function runit() { var prog = document.getElementById("yourcode").value; var mypre = document.getElementById("output"); mypre.innerHTML = ''; Sk.pre = "output"; Sk.configure({output:outf, read:builtinRead}); (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = \'mycanvas\'; var myPromise = Sk.misceval.asyncToPromise(function() { return Sk.importMainWithBody("<stdin>", false, prog, true); }); myPromise.then(function(mod) { console.log(\'success\'); }, function(err) { console.log(err.toString()); });} </script> <h3>Try This</h3> <form> <textarea id="yourcode" cols="40" rows="10">import turtlet = turtle.Turtle()t.forward(100)print "Hello World" </textarea><br /> <button type="button" onclick="runit()">Run</button> </form> <pre id="output" ></pre> <div id="mycanvas"></div> '
+        data = '<h3>Try This</h3> <form> <textarea id="yourcode" cols="40" rows="10">%s</textarea><br /> <button type="button" onclick="runit()">Run</button> </form> <pre id="output" ></pre> <!-- If you want turtle graphics include a canvas --><div id="mycanvas"></div> ' % (value['code'])
         return mark_safe(data)            
 
 
