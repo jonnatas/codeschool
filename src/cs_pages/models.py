a8247b193be708fe2eb65bf92aa9b55f1d96323b
Marcelo <marceloabk@hotmail.com>
2016-05-30 16:25:16 -0300

    creating a block with interactive python code

--- src/cs_pages/models.py
+++ src/cs_pages/models.py
@@ -69,7 +69,16 @@ class InputCodeBlock(CodeBlock):
         lang = value['language'].ref
         code = escape(value['code'])
         data = '<ace-editor mode="%s">%s</ace-editor>' % (lang, code)
-        return mark_safe(data)      
+        return mark_safe(data)
+
+class userInputBlock(CodeBlock):
+    """interative code blocks """
+    
+    def render(self, value):
+        lang = 'python'
+        code = escape(value['code'])
+        data = '<script type="text/javascript"> function outf(text) { var mypre = document.getElementById("output"); mypre.innerHTML = mypre.innerHTML + text; } function builtinRead(x) { if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined) throw "File not found: '" + x + "'"; return Sk.builtinFiles["files"][x];}function runit() { var prog = document.getElementById("yourcode").value; var mypre = document.getElementById("output"); mypre.innerHTML = ''; Sk.pre = "output"; Sk.configure({output:outf, read:builtinRead}); (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = \'mycanvas\'; var myPromise = Sk.misceval.asyncToPromise(function() { return Sk.importMainWithBody("<stdin>", false, prog, true); }); myPromise.then(function(mod) { console.log(\'success\'); }, function(err) { console.log(err.toString()); });} </script> <h3>Try This</h3> <form> <textarea id="yourcode" cols="40" rows="10">import turtlet = turtle.Turtle()t.forward(100)print "Hello World" </textarea><br /> <button type="button" onclick="runit()">Run</button> </form> <pre id="output" ></pre> <div id="mycanvas"></div> '
+        return mark_safe(data)            
 
 
 class HomePage(Page):
@@ -78,6 +87,7 @@ class HomePage(Page):
         ('paragraph', blocks.RichTextBlock()),
         ('code', CodeBlock()),
         ('interactive_code', InputCodeBlock()),
+        ('user_input', userInputBlock()),
         ('image', ImageChooserBlock()),
     ])
 
