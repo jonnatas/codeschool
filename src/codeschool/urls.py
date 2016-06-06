45cc938cec9c7d2b89ed1ec60e7e25865b090645
Paulo Tada <paulohtfs@gmail.com>
2016-06-06 16:47:11 -0300

    Fixing urls and removing migrations of cs_challenge

--- src/codeschool/urls.py
+++ src/codeschool/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     url(r'^questions/', include('cs_questions.urls')),
     url(r'^polls/', include('cs_polls.urls')),
     url(r'^battles/', include('cs_battles.urls', namespace='battle')),
-    url(r'^pbl/', include('cs_pbl.urls', namespace='pbl'))
-    url(r'^challenges/',include('cs_challenge_list.urls')),
+    url(r'^pbl/', include('cs_pbl.urls', namespace='pbl')),
+    url(r'^challenges/',include('cs_challenge_list.urls'))
 ]
 
 
